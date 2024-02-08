4ca7819f1bf79d16db79735da9660b35a078b5ef
selim <selimfatmi@gmail.com>
2024-02-08 13:30:44 +0100

    maj livrable

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function planty_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),array( 'main' ), wp_get_theme()->get('Version') );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),array( 'main' ), time() );
 }
 add_action( 'wp_enqueue_scripts', 'planty_styles');
 
